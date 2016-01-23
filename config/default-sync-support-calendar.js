f25690208bf7161a8e167ae2353d4fe01efbbcae
Nigel Stephen D'Souza <nigelsd@gmail.com>
2016-01-23 00:20:49 +0000

    Adding the hard-linked config/default.js

---
+++ config/default-sync-support-calendar.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+  calendars: {
+    support: {
+      calendarId:     process.env.OB_SUPPORT_ROTA_CALENDAR_ID,
+      usernameSearch: process.env.OB_USERNAME
+    },
+    workPrimary: {
+      calendarId: "primary",
+      attendees: [
+        {
+          email:          process.env.OB_EMAIL_ADDRESS,
+          displayName:    process.env.OB_DISPLAY_NAME,
+          responseStatus: "accepted",
+          self:           true,
+          organizer:      true
+        },
+        {
+          email:       process.env.PERSONAL_EMAIL,
+          displayName: process.env.PERSONAL_DISPLAY_NAME
+        }
+      ]
+    }
+  }
+} 
--- config/default.js
+++ config/default.js
@@ -1,1 +1,35 @@
-../../master-store/configs/default.js
+var cfg   = require('config');
+var defer = require('config/defer').deferConfig;
+
+module.exports = {
+  appName: process.env.npm_package_config_appName,
+
+  auth: {
+    credentialsDir:   process.env.HOME+"/.credentials",
+    clientSecretFile: defer( function (cfg) { return cfg.auth.credentialsDir+"/client_secret.json" } ),
+    tokenFileDir:     defer( function (cfg) { return cfg.auth.credentialsDir } ),
+    tokenFile:        defer( function (cfg) { return "access_token_"+cfg.appName+".json" } ),
+    scopes:           process.env.npm_package_config_googleAuthScopes.split(",")
+  },
+
+  log: {
+    appName: defer(function (cfg) { return cfg.appName } ),
+    level:   "DEBUG",
+    log4jsConfigs: {
+      appenders: [
+        {
+          type:       "file",
+          filename:   defer(function (cfg) { return cfg.log.logDir.concat("/" , cfg.appName , ".log" ) }),
+          category:   defer(function (cfg) { return cfg.appName }),
+          reloadSecs: 60,
+          maxLogSize: 1024000
+        },
+        {
+          type: "console"
+        }
+      ],
+      replaceConsole: true
+    },
+    logDir: "./logs"
+  }
+} 
